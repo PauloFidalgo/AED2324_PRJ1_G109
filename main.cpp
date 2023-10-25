0257bc6bab00554e6eed92a197ee03689c722f92
Paulo Fidalgo <paulofidalgo@PauloFidalgo.Home>
2023-10-25 22:45:44 +0100

    a

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>// Include the header file
 #include "FilesRepository/Manager.h"
+#include "FilesRepository/Menu.h"
 
 int main() {
     Manager manager;
     manager.readFiles();
     manager.numeroEstudantesEmPeloMenosNUCS(7);
+    Menu menu;
+    menu.iniciar();
     return 0;
 }
